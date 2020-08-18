0175a1d519a035638d480ae3f03e404ca19969e3
Vince-Ai <vinceai@outlook.com>
2020-08-18 14:24:02 -0700

    Added Grid Layout
    Added Grid Layout

--- src/Pages/Projects.js
+++ src/Pages/Projects.js
@@ -1,12 +1,72 @@
 import React from 'react'
 import './Projects.css'
 
+let ProjectJson = [
+    {
+        "Name":  "Something2"
+        ,"Img": "imgs/logo_cpp.jpg"
+        ,"Abstract": "Today, I'm going to show you how to use just TWO CSS grid properties to create a fully responsive image gallery"
+        ,"Github": "someurl"
+    },
+    {
+        "Name":  "Something3"
+        ,"Img": "imgs/logo_cpp.jpg"
+        ,"Abstract": "Today, I'm going to show you how to use just TWO CSS grid properties to create a fully responsive image gallery"
+        ,"Github": "someurl"
+    },
+    {
+        "Name":  "Something4"
+        ,"Img": "imgs/logo_cpp.jpg"
+        ,"Abstract": "Today, I'm going to show you how to use just TWO CSS grid properties to create a fully responsive image gallery"
+        ,"Github": "someurl"
+    },
+    {
+        "Name":  "Something5"
+        ,"Img": "imgs/logo_cpp.jpg"
+        ,"Abstract": "Today, I'm going to show you how to use just TWO CSS grid properties to create a fully responsive image gallery"
+        ,"Github": "someurl"
+    },
+    {
+        "Name":  "Something6"
+        ,"Img": "imgs/logo_cpp.jpg"
+        ,"Abstract": "Today, I'm going to show you how to use just TWO CSS grid properties to create a fully responsive image gallery"
+        ,"Github": "someurl"
+    },
+    {
+        "Name":  "Something1"
+        ,"Img": "imgs/logo_cpp.jpg"
+        ,"Abstract": "Today, I'm going to show you how to use just TWO CSS grid properties to create a fully responsive image gallery"
+        ,"Github": "someurl"
+    },
+]
+
+
 export default function Projects (props) {
     return (
-        <div className="H1">Projects</div>
+        <div className="ProjectWrapper">
+            <div className="H1">Projects</div>
+            <main>
+                {ProjectJson.map(proj =>
+                    <Item props={proj} key={proj.Name}/>
+                )}
+            </main>
+        </div>
     );
 }
 
-function Fields (props) {
-    return null;
+/*
+    Item: A single research item
+    Props: Name, Abstract, Img, 
+           Links: [Github, Paper, Poster]
+*/
+function Item (props) {
+    return(
+        <div className="ItemWrapper">
+            <img src={props.props.Img} alt=""/>
+            <div className="Info">
+                <strong className="Name">{props.props.Name}</strong>
+                <p className="Abstract">{props.props.Abstract}</p>
+            </div>
+        </div>
+    );
 }
--- src/Pages/Research.js
+++ src/Pages/Research.js
@@ -1,5 +1,24 @@
 import React from 'react'
 
 export default function Research (props) {
-    return (<div></div>);
+    return (
+        <div>
+            <div className="H1">Research</div>
+            <Item />
+        </div>
+        
+    );
+}
+
+/*
+    Item: A single research item
+    Props: Name, Abstract, Photo, 
+           Links: [Github, Paper, Poster]
+*/
+function Item (props){
+    return (
+        <div className="ItemWrapper">
+
+        </div>
+    );
 }
