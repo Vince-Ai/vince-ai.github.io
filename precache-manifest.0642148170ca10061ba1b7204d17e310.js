7bb218102a6a38aabe0293cbd173e56e2aa270e9
Vince-Ai <vinceai@outlook.com>
2020-08-24 15:53:17 -0700

    Updates

---
+++ precache-manifest.0642148170ca10061ba1b7204d17e310.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e95db64fadc27ff676f7074fb567efb9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ce3b9165446246c0eaa3",
+    "url": "/static/css/main.a67561d5.chunk.css"
+  },
+  {
+    "revision": "0ca131e006aae000008c",
+    "url": "/static/js/2.d941c0fc.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.d941c0fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce3b9165446246c0eaa3",
+    "url": "/static/js/main.71e57b5b.chunk.js"
+  },
+  {
+    "revision": "186e6f1537d221e4110c",
+    "url": "/static/js/runtime-main.dd8a39e1.js"
+  }
+]);
