0007d58e998c8f827dceb023af3cb40339b6fbec
Vince-Ai <vinceai@outlook.com>
2020-09-03 07:14:57 +0800

    Updated Main Page
    Updated Main Page

---
+++ src/Data.js
@@ -0,0 +1,30 @@
+export let edu_en = [
+    {
+        "Img": "",
+        "Name": "The High School Affiliated to Renmin University of China (RDFZ)",
+        "Level": "High School",
+        "Time": "2016.9-2019.7"
+    },
+    {
+        "Img": "",
+        "Name": "University of California, Los Angeles (UCLA)",
+                "Level": "UnderGraduate",
+        "Time": "2019.8-2023.6"
+    },
+];
+
+export let edu_zh = [
+    {
+        "Img": "",
+        "Name": "中国人民大学附属中学（RDFZ）",
+        "Level": "高中",
+        "Time": "2016.9-2019.7"
+    },
+    {
+        "Img": "",
+        "Name": "加州大学洛杉矶分校（UCLA）",
+        "Level": "本科",
+        "Time": "2019.8-2023.6"
+    },
+];
+
--- src/Pages/Main.js
+++ src/Pages/Main.js
@@ -1,19 +1,63 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './Main.css'
+import {edu_zh, edu_en} from '../Data';
+import { GlobalContext } from "../GlobalContext"
 
-export default function Main (props) {
-    return (<Profile />);
+export default function Main () {
+    const {isEng} = useContext(GlobalContext);
+
+    return (
+        <div>
+            <Profile isEng={isEng}/>
+            <Education isEng={isEng}/>
+            <Experience isEng={isEng}/>
+        </div>
+    );
 }
 
-function Profile(props) {
+function Profile (props) {
     return (
         <div className="ProfileWrapper">
-            <h1>Vince Ai</h1>
-            <h3>{"Computer Science and Applied Mathematics,\n Samueli School of Engineering, UCLA"}</h3>
+            <h1>{props.isEng ? "Vince Ai" : "艾文斯"}</h1>
+            <h3>{props.isEng ? 
+                "Computer Science and Applied Mathematics, Samueli School of Engineering and Applied Science, UCLA"
+                : "计算机与应用数学专业，加州大学洛杉矶分校，工程与应用科学学院"
+            }</h3>
             <h5>4245359709</h5>
             <div className="contact">
                 
             </div>
         </div>
     )
+}
+
+function Education (props) {
+    function Field (props) {
+        return (
+            <div>
+                <img src={props.data.Img} alt=""/>
+                <p>{props.data.Name}</p>
+                <p>{props.data.Level}</p>
+                <p>{props.data.Time}</p>
+            </div>
+        );
+    }
+
+    let cur_edu = props.isEng ? edu_en : edu_zh;
+
+    return (
+        <div>
+            <strong>{props.isEng ? "Education" : "学历"}</strong>
+            {cur_edu.map(edu => <Field key={edu.Name} data={edu}/>)}
+        </div>
+    );
+}
+
+
+function Experience () {
+    return (
+        <div>
+
+        </div>
+    );
 }
